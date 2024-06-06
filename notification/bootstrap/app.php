304e977f32ba20fabe88269a04335a4534af32f4
Eray Aydın <erayaydinn@protonmail.com>
2024-06-06 17:03:19 +0300

    feat: implement notification service

---
+++ notification/bootstrap/app.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Foundation\Application;
+use Illuminate\Foundation\Configuration\Exceptions;
+use Illuminate\Foundation\Configuration\Middleware;
+use function Illuminate\Filesystem\join_paths;
+
+$app = Application::configure(basePath: dirname(__DIR__))
+    ->withMiddleware(function (Middleware $middleware) {
+        //
+    })
+    ->withExceptions(function (Exceptions $exceptions) {
+        //
+    })->create();
+
+$app->useAppPath(join_paths(dirname(__DIR__), 'src'));
+
+return $app;
